94cc25c4e62b55c39dc875c128ece0ac7b5679c6
yilverdeja <yilverdeja@gmail.com>
2021-02-20 01:28:20 +0800

    dark mode added
    and dataRetrieved variable implemented to only load data on screen when data has been retrieved

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import BootstrapVue from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import { firestorePlugin } from "vuefire"
 
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
